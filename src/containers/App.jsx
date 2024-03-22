0430026a9edd7b35c654b5298aa6a5e32d7279f4
CrazyDevilJohn <7425264@gmail.com>
2024-03-22 19:04:24 +0530

    projact initiated

---
+++ src/containers/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Header } from "./";
+
+const App = () => {
+  return (
+    <div className="w-full xl:w-[1600px] py-32 px-4 lg:px-12 pr-4 lg:pr-32">
+      {/* partical */}
+
+      {/* header */}
+      <Header />
+      {/* home container */}
+
+      {/* about container */}
+
+      {/* skills container */}
+
+      {/* projects container] */}
+
+      {/* contact container */}
+
+      {/* footer container */}
+    </div>
+  );
+};
+
+export default App;
